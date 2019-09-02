bfe5daa079c57b32e3871c2e24f2c986c90e1d47
kjpmj <kjpmj@naver.com>
2019-09-02 16:43:29 +0900

    api 서버 연동

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import TodoList from './components/TodoList';
 import AddTodoForm from './components/AddTodoForm';
 import Filter from './components/Filter';
 import SearchTodo from './components/SearchTodo';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+    
+    axios.get('/todos')
+      .then((resp)=>{
+        console.log(resp);
+      });
+
     return (
       <div>
         <SearchTodo/>
